ac686574b4fe610fa9b52f926a31d42c72bed3ed
GianProjekte <gian.rathgeb@bluewin.ch>
2018-12-30 23:16:48 +0100

    Primzahlentester hinzugefühgt, auch in der Tabelle

---
+++ unterseiten/Primzahlentester.php
@@ -0,0 +1,60 @@
+<?php
+
+    echo "Hallo ".$_GET['name']."";
+
+    if(isInteger($_GET)['nummer']) && $_GET['nummer'] > 0){
+
+        if(isprime($_GET['nummer'])){
+
+            echo "<p>".$_GET['nummer']." ist eine Primzahl</p>";
+
+        }else {
+
+            echo "<p>".$_GET['nummer']." ist keine Primzahl</p>";
+
+        }
+
+    }else {
+
+        echo "<p>Bitte trage eine Zahl ein!";
+
+    }
+
+
+    function isInteger($input) {
+
+        return(ctype_digit(strval($input)));
+
+    }
+
+    function isprime($primetest) {
+
+        $maxtest = sqrt($primetest);
+        for ($i = 2; $i <= $maxtest, ++$i) {
+
+            if ($primetest % $i == 0) {
+
+                return false;
+
+            }
+
+        }
+
+        return true;
+
+    }
+
+
+?>
+
+    <h1>Primzahlentester</h1>
+
+<p>Trage bitte eine ganzzahlige Zahl ein, welche dann überprüft wird.</p>
+
+<form>
+
+    <input type="text" name="nummer">
+
+    <input type="submit" value="Testen!">
+
+</form>
